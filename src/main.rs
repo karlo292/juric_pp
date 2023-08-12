11c55fefe15afdcf2e57dca9ea608e8a6f8cd8d8
Andrej Bartulin <andrej.bartulinvr@gmail.com>
2023-08-12 13:17:08 +0200

    Init commit

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use std::io::{stdout, Stdout};
+use std::{env, process::exit};
+
+use crossterm::{style, style::Color, terminal, QueueableCommand, execute};
+use crossterm::cursor::MoveTo;
+
+
+fn main() {
+    clearscreen::clear().expect("Failed to clean screen!");
+    let mut stdout: Stdout = stdout();
+
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 3 {
+        println!("Too few arguments passed!");
+        println!("Usage: ./cargo run --release <distro> <package>");
+
+        exit(0);
+    }
+
+    let warning_1: String = String::from("Juric++ is not offical package manager of your distribution. ringwormGO does not gurrant safety of your files!");
+    let warning_2: String = String::from("User discretion advised!");
+
+    let x: u16 = terminal::size().unwrap().0;
+    stdout.queue(style::SetForegroundColor(Color::Red)).expect("Failed to set foreground color!");
+
+    execute!(stdout, MoveTo((x / 2) - (u16::try_from(warning_1.len() / 2).unwrap()), 0)).expect("Failed to move cursor!");
+    println!("{}", warning_1);
+
+    execute!(stdout, MoveTo((x / 2) - (u16::try_from(warning_2.len() / 2).unwrap()), 1)).expect("Failed to move cursor!");
+    println!("{}", warning_2);
+
+    stdout.queue(style::SetForegroundColor(Color::Reset)).expect("Failed to set/reste foreground color!");
+}
