8b82e2055d37576ee6651574849ad9d0679f22d5
Andrej Bartulin <andrej.bartulinvr@gmail.com>
2023-08-12 17:28:36 +0200

    Update

---
+++ README.md
@@ -0,0 +1,7 @@
+# Juric++
+Juric rewrite in Rust
+
+We'll use web scraping to extract needed data to download & install files
+
+# **Juric++ is not offical package manager of your distribution. ringwormGO does not gurrant safety of your files!**
+# **User discretion advised!**
--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,15 @@ use std::{env, process::exit};
 use crossterm::{style, style::Color, terminal, QueueableCommand, execute};
 use crossterm::cursor::MoveTo;
 
-
 fn main() {
     clearscreen::clear().expect("Failed to clean screen!");
     let mut stdout: Stdout = stdout();
 
     let args: Vec<String> = env::args().collect();
 
-    if args.len() < 3 {
+    if args.len() < 4 {
         println!("Too few arguments passed!");
-        println!("Usage: ./cargo run --release <distro> <package>");
+        println!("Usage: ./cargo run --release <distro> <architecture> <package>");
 
         exit(0);
     }
@@ -30,5 +29,13 @@ fn main() {
     execute!(stdout, MoveTo((x / 2) - (u16::try_from(warning_2.len() / 2).unwrap()), 1)).expect("Failed to move cursor!");
     println!("{}", warning_2);
 
-    stdout.queue(style::SetForegroundColor(Color::Reset)).expect("Failed to set/reste foreground color!");
+    stdout.queue(style::SetForegroundColor(Color::Reset)).expect("Failed to set/reset foreground color!");
+
+    println!("Gathering informations...");
+    println!("    OS: {} | Requested OS: {}", whoami::distro().to_string(), &args[1]);
+    println!("    Architecture requested: {}", &args[2]);
+    println!("    Package requested: {}", &args[3]);
+
+    // web scrape data
+    // download & install files
 }
