9b93d7931df13c9635cc46f64b4234bbf9ca4398
karlo292 <64750254+karlo292@users.noreply.github.com>
2023-10-10 14:35:24 +0200

    Update utilities.rs

--- src/utilities.rs
+++ src/utilities.rs
@@ -2,26 +2,38 @@ use std::env;
 use std::fs;
 use std::path::Path;
 
-const PATH: &str = ".juric_temp";
+// Define a constant for the temporary folder name
+const TEMP_FOLDER: &str = ".juric_temp";
 
-pub fn cd_to_folder(folder: &String) {
-    let _path = Path::new(folder);
-    env::set_current_dir(&_path).expect("Failed to change directory!");
+// Change the current directory to the specified folder
+pub fn cd_to_folder(folder: &str) {
+    set_current_dir(folder);
 }
 
+// Change the current directory to the temporary folder
 pub fn cd_to_temp_folder() {
-    let _path = Path::new(PATH);
-    env::set_current_dir(&_path).expect("Failed to change directory!");
+    set_current_dir(TEMP_FOLDER);
 }
 
-pub fn create_folder(folder: &String) {
-    if !Path::new(folder).is_dir() {
-        fs::create_dir(folder).expect("Failed to create directory")
-    }
+// Create a new folder if it doesn't already exist
+pub fn create_folder(folder: &str) {
+    create_directory(folder);
 }
 
+// Create the temporary folder if it doesn't already exist
 pub fn create_temp_folder() {
-    if !Path::new(PATH).is_dir() {
-        fs::create_dir(PATH).expect("Failed to create directory")
+    create_directory(TEMP_FOLDER);
+}
+
+// Set the current working directory to the specified directory
+fn set_current_dir(dir: &str) {
+    let path = Path::new(dir);
+    env::set_current_dir(path).expect("Failed to change directory!");
+}
+
+// Create a directory if it doesn't already exist
+fn create_directory(dir: &str) {
+    if !Path::new(dir).is_dir() {
+        fs::create_dir(dir).expect("Failed to create directory");
     }
 }
